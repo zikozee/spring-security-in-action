de9a036b002ec2334112233e7801b6779e502a34
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2021-12-02 18:05:38 +0100

    added new service user-details-service

---
+++ section-3/user-details-service/src/test/java/com/zikozee/userdetailsservice/UserDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zikozee.userdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

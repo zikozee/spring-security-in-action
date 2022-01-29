03851dffe1aedb4b542f61d0794bd0209b46c42b
ezekiel.eromosei <ezekiel.eromosei@gmail.com>
2022-01-29 21:20:56 +0100

    implement scenario one base files

---
+++ section-11/business-logic-server-scenario-one/src/main/java/com/ezikozee/businesslogicserverscenarioone/BusinessLogicServerScenarioOneApplication.java
@@ -0,0 +1,13 @@
+package com.ezikozee.businesslogicserverscenarioone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessLogicServerScenarioOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessLogicServerScenarioOneApplication.class, args);
+    }
+
+}
---
+++ section-11/business-logic-server-scenario-one/src/test/java/com/ezikozee/businesslogicserverscenarioone/BusinessLogicServerScenarioOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezikozee.businesslogicserverscenarioone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicServerScenarioOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
